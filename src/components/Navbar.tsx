8921cb5f8684cfb052e914e06b955923a5a1a978
FaridAquino <farid.aquino@utec.edu.pe>
2025-06-18 11:14:56 -0500

    feat: añadir botones al navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,7 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Navbar() {
+	const navigate = useNavigate();
 	function logout() {
 		localStorage.removeItem("token");
 		location.reload();
@@ -6,7 +9,12 @@ export default function Navbar() {
 
 	if (localStorage.getItem("token")) {
 		return (
+
 			<div className="">
+				<section className="flex justify-center items-center py-4 gap-10">
+					<button onClick={() => navigate("/auth/login")} className="bg-blue-500">Iniciar sesion</button>
+					<button onClick={() => navigate("/auth/register")} className="bg-blue-500">Registrarse</button>
+				</section>
 				<div className="text-2xl">Uber</div>
 				<button id="logout" onClick={logout}>
 					Logout
@@ -14,6 +22,12 @@ export default function Navbar() {
 			</div>
 		);
 	} else {
-		return <div className="">Uber</div>;
+		return (
+			<div className="">Uber
+				<section className="flex justify-center items-center py-4 gap-10">
+					<button onClick={() => navigate("/auth/login")} className="bg-blue-500">Iniciar sesion</button>
+					<button onClick={() => navigate("/auth/register")} className="bg-blue-500">Registrarse</button>
+				</section>
+			</div>);
 	}
 }
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -6,11 +6,6 @@ export default function LoginPage() {
 	return (
 		<main className="px-10 felx flex-col items-center justify-center">
 
-			<section className="flex justify-center items-center py-4 gap-10">
-				<button onClick={() => navigate("/auth/login")} className="bg-blue-500">Iniciar sesion</button>
-				<button onClick={() => navigate("/auth/register")} className="bg-blue-500">Registrarse</button>
-			</section>
-
 			<article className="flex justify-between">
 				<LoginForm />
 			</article>
