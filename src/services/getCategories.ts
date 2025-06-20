26966ccb384007d29b8a4bf423ee8c5ec3761fef
stiffis <steve.ildefonso@utec.edu.pe>
2025-06-19 20:43:31 -0500

    feat: format

--- src/services/getCategories.ts
+++ src/services/getCategories.ts
@@ -2,10 +2,9 @@ import CategoryResponse from "@interfaces/expenses/CategoryResponse";
 import Api from "@services/api";
 
 export async function getCategories() {
-    const api = await Api.getInstance();
-    const response = await api.get<NonElementParentNode, CategoryResponse[]>({
-        url: "/expenses_category"
-    });
-    return response;
+	const api = await Api.getInstance();
+	const response = await api.get<NonElementParentNode, CategoryResponse[]>({
+		url: "/expenses_category",
+	});
+	return response;
 }
-
